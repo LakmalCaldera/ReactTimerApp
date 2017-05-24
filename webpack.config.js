627191fdeab470bcc5fca3880a96809ff78ae543
Lakmal Caldera <lakmal.developer@gmail.com>
2017-05-24 19:37:43 +0530

    added timer and coutdown component

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,9 @@ module.exports = {
     alias: {
       Main: 'app/components/Main.jsx',
       applicationStyles: 'app/styles/app.scss',
-      Navigation: 'app/components/Navigation.jsx'
+      Navigation: 'app/components/Navigation.jsx',
+      Countdown: 'app/components/Countdown.jsx',
+      Timer: 'app/components/Timer.jsx'
     }
   },
   module: {
